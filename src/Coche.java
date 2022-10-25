5b587c5afe6863ad5083f567a787cdf6f2c99db6
MariaSuarez <rfb52455@educastur.es>
2022-10-25 09:49:20 +0200

    coches commit 2 getters y setters

--- src/Coche.java
+++ src/Coche.java
@@ -2,4 +2,12 @@
 public class Coche {
 	String matricula;
 
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
 }
