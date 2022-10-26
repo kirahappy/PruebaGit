07ec517a662cb506d910964e9a41b5cee8df5d37
GerardLaviana <gerardlaviana@gmail.com>
2022-10-26 12:02:03 +0200

    Constructor Getter y Setters Creados clase Tienda - Gerard

--- src/Tienda.java
+++ src/Tienda.java
@@ -2,4 +2,24 @@
 public class Tienda {
 	private String nombre;
 	private String propietario;
+	
+	public Tienda(String nombre, String propietario) {
+		this.nombre = nombre;
+		this.propietario = propietario;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPropietario() {
+		return propietario;
+	}
+	public void setPropietario(String propietario) {
+		this.propietario = propietario;
+	}
+	
+	
 }
